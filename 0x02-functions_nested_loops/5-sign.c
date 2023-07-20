8302bbf29f077d1bcd7eabf13b21f615871ebdaf
TommyM1 <popoolaajayio@gmail.com>
2023-07-20 03:19:45 -0700

    My first commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- *         0 and prints 0 if n is zero
- *        -1 and prints - if n is less than zero
- */
+  * print_sign - Prints the sign of a number
+  * @n: Number to be printed
+  *
+  * Return: 1 if n > 0, 0 if n==0 or -1 if n<0.
+  *
+  * Description: The function would print + if n>0, 0 if equal
+  * and -1 if negative
+  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -19,14 +22,9 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
 	}
-
-	/* This return statement will handle the case when none of the if-else conditions are met.
-	 * You can choose the appropriate value to return here based on your requirements.
-	 * For example, returning 0 here to indicate an error or undefined state. */
-	return (0);
 }
