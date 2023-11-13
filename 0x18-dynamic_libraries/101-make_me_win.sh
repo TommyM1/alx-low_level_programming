b7f0f5fb680263a87d5ab89a2f327e8d8f16b8d3
TommyM1 <popoolaajayio@gmail.com>
2023-11-13 13:38:21 -0800

    libhack update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://rawgithubusercontent.com/TommyM1/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="PWD/../libhack.so"
+wget -P .. https://rawgithubusercontent.com/TommyM1/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="PWD/../libgiga.so"
